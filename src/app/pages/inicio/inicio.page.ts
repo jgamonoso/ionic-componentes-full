d8c2f5d4aa51d89404cac4bb4c7aa26fc7bbb04d
Juan Gamonoso <juan.gamonoso@stratesys-ts.com>
2022-06-22 12:38:08 +0200

    task 49

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -44,6 +44,11 @@ export class InicioPage implements OnInit {
       name: 'Checkbox',
       redirectTo: '/check'
     },
+    {
+      icon: 'calendar-outline',
+      name: 'DateTime',
+      redirectTo: '/date-time'
+    },
   ];
 
   constructor() { }
