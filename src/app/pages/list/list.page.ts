477ea96c5e128edbe0b77a89c3a26852237620c1
Juan Gamonoso <juan.gamonoso@stratesys-ts.com>
2022-06-22 18:24:19 +0200

    task 58

---
+++ src/app/pages/list/list.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.page.html',
+  styleUrls: ['./list.page.scss'],
+})
+export class ListPage implements OnInit {
+
+  usuarios: Observable<any>;
+
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  ngOnInit() {
+    // this.dataService.getUsuarios().subscribe(console.log)
+
+    this.usuarios = this.dataService.getUsuarios();
+  }
+
+}
